f31f2366d146b3f2507207dbaaeb8c7ca304e30c
tanvir <himonislamfre@gmail.com>
2022-05-08 21:00:14 +0600

    blog commit

--- src/Home/Section/Section.js
+++ src/Home/Section/Section.js
@@ -11,14 +11,14 @@ const Section = () => {
                 </div>
                 <div className='section-details'>
                     <h1>Best Perfume</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ratione distinctio asperiores officiis recusandae labore aut velit ipsa sit sunt.</p>
+                    <p>We are crossing an invisible line this week, which will allow us to say goodbye to the convoluted past, and start planning for a much more purposeful future.</p>
                 </div>
 
             </div>
             <div className='section-container2'>
                 <div className='section-details'>
                     <h1>Best price</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni non obcaecati placeat iste aspernatur aliquam harum sunt laudantium, sit recusandae</p>
+                    <p>You may have been involved in a particular purchase, and it’s still in the testing phase. Try to prolong the testing and tasting unless you already know</p>
                 </div>
                 <div>
                     <img src={image2} alt="" />

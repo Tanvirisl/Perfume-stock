6f1b179668e14e48e253bf38790a41a412b51ecb
tanvir <himonislamfre@gmail.com>
2022-05-08 21:16:33 +0600

    commit

--- src/Inventorys/Inventorys.js
+++ src/Inventorys/Inventorys.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import useService from '../Hooks/useService';
 import InventoryDetails from '../InventoryDetails/InventoryDetails';
-
+import './Inventory.css'
 const Inventorys = () => {
     const [service] = useService();
     console.log(service)
     return (
         <div>
-            <h2>this is inventory:{service.length}</h2>
+            <h2 className='inventory-title'>This is inventory:{service.length}</h2>
             {
                 service.map(services => <InventoryDetails
                     key={services._id}
